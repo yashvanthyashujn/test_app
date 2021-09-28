503e8501d4b3fe527fcbda0f102f2403032b7d00
Yashwanth <yashvanthyashujnvt@gmail.com>
2021-09-28 15:11:11 +0530

    create user table model

--- db/schema.rb
+++ db/schema.rb
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2021_09_28_051513) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
+    t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
